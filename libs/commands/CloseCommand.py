d6cd0db144637c7145e7e5e3faf19056bb4291e7
RONALD FABRIZIO HURTADO RAMOS <maxx1212_2@hotmail.com>
2024-10-05 18:23:32 -0500

    Add Commands

---
+++ libs/commands/CloseCommand.py
@@ -0,0 +1,22 @@
+import json
+
+from libs.commands.ICommand import ICommand
+from libs.logger import logger
+from libs.config import config
+from libs.OutMessageManager import OutMessageManager
+
+
+class CloseCommand(ICommand):
+
+    async def execute(self, message: str) -> bool:
+        try:
+            message = OutMessageManager().close_command_format(message)
+            if message == {}:
+                return False
+            message_to_send = json.dumps(message) + "\n"
+            config.writer.write(message_to_send.encode("utf-8"))
+            await config.writer.drain()
+            return True
+        except Exception as e:
+            logger.error(f"Error en Close Command: {e}")
+            return False
