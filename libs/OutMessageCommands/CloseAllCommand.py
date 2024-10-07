d26039f59fae076661d8a536727bde26b9c50e7d
RONALD FABRIZIO HURTADO RAMOS <maxx1212_2@hotmail.com>
2024-10-07 15:07:44 -0500

    feat: add new commands
    add close all, margin level and open positions

---
+++ libs/OutMessageCommands/CloseAllCommand.py
@@ -0,0 +1,21 @@
+from libs.config import config
+import json
+from libs.OutMessageCommands.ICommand import ICommand
+from libs.logger import logger
+from libs.OutMessageManager import OutMessageManager
+
+
+class CloseAllCommand(ICommand):
+
+    async def execute(self, message: str) -> bool:
+        try:
+            message = OutMessageManager().close_all_command_format(message)
+            if message == {}:
+                return False
+            message_to_send = json.dumps(message) + "\n"
+            config.writer.write(message_to_send.encode("utf-8"))
+            await config.writer.drain()
+            return True
+        except Exception as e:
+            logger.error(f"Error en Info Command: {e}")
+            return False
---
+++ libs/OutMessageCommands/OpenPositionsCommand.py
@@ -0,0 +1,23 @@
+from libs.config import config
+import json
+from libs.OutMessageCommands.ICommand import ICommand
+from libs.logger import logger
+from libs.OutMessageManager import OutMessageManager
+
+
+class OpenPositionsCommand(ICommand):
+
+    async def execute(self, message: str) -> bool:
+        try:
+            message = OutMessageManager().open_positions_command_format(
+                message
+            )
+            if message == {}:
+                return False
+            message_to_send = json.dumps(message) + "\n"
+            config.writer.write(message_to_send.encode("utf-8"))
+            await config.writer.drain()
+            return True
+        except Exception as e:
+            logger.error(f"Error en Info Command: {e}")
+            return False
